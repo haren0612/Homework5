7ca03acdb8a4b9bb8556fd52d98752764f35bcf1
haren0612 <haren0612@gmail.com>
2024-02-29 02:53:40 -0500

    added plugins and improved the calc app

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Created a new repo Homework5 and updated my last calc code in Assignment4 by adding REPL and CommandPatterns
 
+Created a side branch named plugins and as per the branch name , added plugins functionality to the updated calc app and merged it with the main branch using pull request
--- app/commands/menu/__init__.py
+++ app/commands/menu/__init__.py
@@ -1,14 +1,10 @@
-from app.commandhandler import CommandHandler
-
-
 class MenuCommand:
-    def __init__(self):
-        self.command_handler = CommandHandler()
+    def __init__(self, available_commands):
+        self.available_commands = available_commands
 
-
-    def display_menu(self):
-        print("\nAvailable commands:")
-        for command in self.command_handler.command_list:
+    def execute(self):
+        print("Commands available:")
+        print(" - menu")
+        for command in self.available_commands:
             print(f" - {command}")
-        print(" - exit (to quit the application)")
-        print("Enter command in the format: command operand1 operand2")
+        
